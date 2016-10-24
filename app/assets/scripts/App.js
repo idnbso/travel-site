2cf2b525f859b8512593a76715f9a4e42de14b9a
Idan <idanbusso@gmail.com>
2016-10-25 00:21:45 +0300

    Completed implementing sticky header and links highlighting and navigation.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
-import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 var features = new RevealOnScroll($('.feature-item'), '85%');
 var testimonials = new RevealOnScroll($('.testimonial'), '60%');
+var stickyHeader = new StickyHeader();
